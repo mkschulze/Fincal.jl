558d5d8540f6d188c362862fd8311d6a490c2fa7
mksschulze <mks@e.mail.de>
2022-09-11 15:42:38 +0200

    init commit

---
+++ src/income.jl
@@ -0,0 +1,16 @@
+export income_statement
+
+"""
+Umsatzerlöse
+sonstige Erträge
+Materialaufwand
+Personalaufwand
+Abschreibungen
+sonstige Aufwendungen
+Steuern
+Jahresüberschuss/Jahresfehlbetrag
+"""
+
+function income_statement()
+    
+end
---
+++ src/profit_metrics.jl
@@ -0,0 +1,34 @@
+export rentability, return_on_equity, debt_returns, return_on_investment, capital_turnover, sales_proftability, wacc
+
+
+### PROFITABILITY METRICS ###
+
+# https://studyflix.de/wirtschaft/thema/kennzahlen-der-bwl-79https://studyflix.de/wirtschaft/liquiditat-1870
+
+function rentability() # Rentabilität
+
+end
+
+function return_on_equity() # Eigenkapitalrentabilität
+
+end
+
+function debt_returns() # Fremdkapitalrentabilität
+
+end
+
+function return_on_investment() # Gesamtkapitalrentabilität
+
+end
+
+function capital_turnover() # Kapitalumschlag
+
+end
+
+function sales_proftability() # Umsatzrentabilität
+
+end
+
+function wacc() # Gewichtete durchschnittliche Kapitalkosten
+ 
+end
